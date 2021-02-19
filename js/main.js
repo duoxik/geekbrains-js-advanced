71be6511a0f12fae49c70173dd8080ce66b51ecc
Моисеев Владислав Игоревич <moiseev.v.ig@sberbank.ru>
2021-02-19 18:48:33 +0300

    hw lesson_3

--- js/main.js
+++ js/main.js
@@ -1,132 +1,60 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 }
-];
+const API_URL =
+  "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
 
-const renderGoodsItem = (title, price) => {
-    return `<div class="products__item">
-                <div class="products__item_cover">
-                    <a href="#" class="products__item_cover_button">
-                        <img src="images/products/button.svg" alt="image">
-                        <p class="products__item_cover_text">Add to Cart</p>
-                    </a>
-                </div>
-                <img src="images/products/item__1.png" alt="image">
-                <div class="products__item_header">${title}</div>
-                <div class="products__item_price">${price} $</div>
-            </div>`;
-};
-
-const renderGoodsList = () => {
-    const items = document.querySelector('.products__items');
-    goods.forEach(item => items.insertAdjacentHTML('beforeend', renderGoodsItem(item.title, item.price)));
-};
-
-renderGoodsList();
-
-/* Домашка к уроку 2 */
-/* 1. не стал выносить size потому что он может быть не у всех товаров */
-/* 2. помечаю поля как private по аналогии с парадигмой инкапсуляции в java */
-/*    насколько это уместно здесь не знаю :) */
 class ProductsCart {
-    #products = new Array();
-    #totalPrice = 0; 
+  products = new Array();
+  totalPrice = 0;
 
-    addProduct(product) {
-        if (!(product instanceof Product)) {
-            throw new Error('Передан параметр не являющийся инстансом Product');
-        }
-
-        this.#products.push(product);
-        this.#totalPrice += product.getPrice();
+  add(product) {
+    if (!(product instanceof Product)) {
+      throw new Error("Передан параметр не являющийся инстансом Product");
     }
 
-    getTotalPrice() {
-        return this.#totalPrice;
-    }
+    this.products.push(product);
+    this.totalPrice += product.price;
+  }
+
+  render() {
+    const productItems = document.querySelector(".products__items");
+    this.products.forEach((product) => {
+      const productItem = new Product(product.product_name, product.price);
+      productItems.insertAdjacentHTML("beforeend", productItem.render());
+    });
+  }
+
+  fill() {
+    fetch(`${API_URL}/catalogData.json`)
+      .then((response) => response.json())
+      .then((products) => {
+        this.products = products;
+        this.render();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 }
 
 class Product {
-    #name;
-    #price;
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
 
-    constructor(name, price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    getName() {
-        return this.name;
-    }
-
-    getPrice() {
-        return this.price;
-    }
+  render() {
+    return `<div class="products__item">
+                  <div class="products__item_cover">
+                      <a href="#" class="products__item_cover_button">
+                          <img src="images/products/button.svg" alt="image">
+                          <p class="products__item_cover_text">Add to Cart</p>
+                      </a>
+                  </div>
+                  <img src="images/products/item__1.png" alt="image">
+                  <div class="products__item_header">${this.title}</div>
+                  <div class="products__item_price">${this.price} $</div>
+              </div>`;
+  }
 }
 
-class Shirt extends Product {
-    #size;
-    
-    constructor(name, price, size) {
-        super(name, price);
-        this.size = size;
-    }
-
-    getSize() {
-        return this.size;
-    }
-}
-
-class Socks extends Product {
-    #size;
-
-    constructor(name, price, size) {
-        super(name, price);
-        this.size = size;
-    }
-
-    getSize() {
-        return this.size;
-    }
-}
-
-class Jacket extends Product {
-    #size;
-
-    constructor(name, price, size) {
-        super(name, price);
-        this.size = size;
-    }
-
-    getSize() {
-        return this.size;
-    }
-}
-
-class Shoes extends Product {
-    #size;
-
-    constructor(name, price, size) {
-        super(name, price);
-        this.size = size;
-    };
-
-    getSize() {
-        return this.size;
-    };
-};
-
-shirt = new Shirt('test shirt', 250, 'L');
-socks = new Socks('test socks', 100, 'S');
-jacket = new Jacket('test jacket', 1000, 'XL');
-shoes = new Shoes('test shoes', 500, 'M');
-
-cart = new ProductsCart();
-cart.addProduct(shirt);
-cart.addProduct(socks);
-cart.addProduct(jacket);
-cart.addProduct(shoes);
-
-console.log(cart);
+const cart = new ProductsCart();
+cart.fill();
